66050efdc5a7591b9e5d5e518514dd793b6c1ce2
yashbhoomkar <18298668-yashbhoomkar@users.noreply.replit.com>
2025-04-11 17:19:33 +0000

    added tests

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -1,5 +1,8 @@
 
 #include <gtest/gtest.h>
+int sideEffect = 32;
+
+
 
 TEST(SampleTest, SimpleAssertion) {
     EXPECT_EQ(1 + 1, 2);
@@ -9,6 +12,16 @@ TEST(SampleTest, SimpleAssertion) {
 TEST(ExampleTest, DemonstrateGTestMacros)
 {
     EXPECT_TRUE(true);
+}
+
+bool test_function(int x)
+{
+    return x > 0;
+}
+
+TEST(ExampleTest, test_function_test)
+{
+    EXPECT_TRUE(test_function(1));
 }
 
 int main(int argc, char **argv)
