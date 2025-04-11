082b7d5db7df1c66fa27f1716b09e8d0dab0f38c
yashbhoomkar <18298668-yashbhoomkar@users.noreply.replit.com>
2025-04-11 18:50:06 +0000

    added text fixture

--- test/sample_test.cpp
+++ test/sample_test.cpp
@@ -24,8 +24,49 @@ TEST(ExampleTest , asser_test)
     EXPECT_LE(1, 1); // less than or equal
 }
 
+// text fixture
 
+class Demo
+{
+    public:
+    int val;
+    Demo()
+    {
+        val = 0;
+    }
+    Demo(int x)
+    {
+        val = x;
+    }
 
+    void increment(int x)
+    {
+        this->val += x;
+    }
+};
+
+// fixture class
+class DemoClassFixture : public ::testing::Test
+{
+    public:
+    Demo * obj;
+    void setup()
+    {
+        obj = new Demo(10);
+    }
+
+    void destroy()
+    {
+        delete obj;
+    }
+};
+
+// test case 1
+TEST_F(DemoClassFixture, testing)
+{
+    obj->increment(10);
+    EXPECT_EQ(obj->val, 20);
+}
 
 
 int main(int argc, char **argv)
@@ -33,9 +74,3 @@ int main(int argc, char **argv)
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
-
-
-
-
-
-
